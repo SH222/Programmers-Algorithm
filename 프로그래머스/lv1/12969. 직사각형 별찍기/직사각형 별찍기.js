e7732bb166f3ccac1dc9cb32e59a328c6b7dece4
승현 <67587253+SH222@users.noreply.github.com>
2023-02-24 21:43:37 +0900

    [level 1] Title: 직사각형 별찍기, Time: 60.25 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,11 +2,15 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    let answer = "";
-    for(let i = 0; i < b; i++){
-        for(let j = 0; j < a; j++) answer += "*";
-        answer += "\n";
+    // let answer = "";
+    // for(let i = 0; i < b; i++){
+    //     for(let j = 0; j < a; j++) answer += "*";
+    //     answer += "\n";
+    // }
+    // console.log(answer);
+    
+    // repeat 활용
+    for(let i=0; i<b; i++){
+        console.log(("*").repeat(a));
     }
-    console.log(answer);
-    // repeat 활용하기
 });
